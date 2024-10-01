d07d97a420e3f03b4702ff5f4f754d617785c955
Ismael Contreras Michel <ismaelc@sforce.local>
2024-10-01 09:52:29 -0400

    fix: .env

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,7 +1,8 @@
-import "dotenv/config"
 import appConfig from "@/configs/app.config";
 import databaseConfig  from "@/configs/database.config";
 import {authConfig} from "@/configs/auth.config";
 import validationConfig from "@/configs/validation.config";
+import { config } from "dotenv";
 
+config({path: `${process.cwd()}/.env`});
 export {appConfig, databaseConfig, authConfig, validationConfig}
